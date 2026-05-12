a11d7dcfbe20b73edeb4f5e45607775c24ccc274
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-05-13 03:45:39 +0600

    Repository Pattern and UnitOfWorks Pattern implementations

---
+++ src/practice.Web/Codes/UnitOfWorks.cs
@@ -0,0 +1,26 @@
+﻿namespace Practice.Web.Codes
+{
+
+
+    // This class is responsible for managing database transactions and ensuring that all operations are executed as a single unit of work.
+    public class UnitOfWorks
+    {
+
+        public void Insert(object entity)
+        {
+            // Code to insert entity into the database
+        }
+        public void Update(object entity)
+        {
+            // Code to update entity in the database
+        }
+        public void Delete(object entity)
+        {
+            // Code to delete entity from the database
+        }
+        public void save()
+        {
+            // Code to commit all changes to the database
+        }
+    }
+}
