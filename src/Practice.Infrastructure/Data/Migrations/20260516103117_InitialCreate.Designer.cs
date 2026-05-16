d6c4ead3df09860a174b77862b6a18a71933df26
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-05-16 17:11:29 +0600

    Checking migrations

--- src/Practice.Infrastructure/Data/Migrations/20260516103117_InitialCreate.Designer.cs
+++ src/Practice.Infrastructure/Data/Migrations/20260516103117_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Practice.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using Practice.Infrastructure.Data;
 namespace Practice.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260516103117_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
