7d444fc70b1b135b59af2564c0801b5f80ad9739
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-05-20 19:04:34 +0600

    Reflection Topics.csproj

---
+++ CSharp Topics/CSharp Topics/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReflectionTopics
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+
+        public double GetDiscountedPrice()
+        {
+            return Price * 0.9; // 10% discount
+        }
+    }
+
+    /**
+     * {
+     *     name : null,
+     *     price: 0.0,
+     *     description: null
+     * 
+     * }
+     * 
+     */
+}
---
+++ CSharp Topics/CSharp Topics/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using ReflectionTopics;
+using System.Reflection;
+using System.Text.Json;
+
+Type t;
+
+Assembly a; // collection of Type
+
+
+Type tp = typeof(Product);
+Type ti = typeof(int);
+
+
+MethodInfo[] methods = tp.GetMethods();
+
+foreach (var method in methods)
+    Console.WriteLine(method.Name);
+
+MethodInfo myMethod = tp.GetMethod("GetDiscountedPrice");
+
+
+ConstructorInfo constructor = tp.GetConstructor(new Type[] { });
+
+PropertyInfo property = tp.GetProperty("Price");
+
+object o = constructor.Invoke(new object[] { });
+
+property.SetValue(o, 100);
+
+
+object result = myMethod.Invoke(o, new object[] { });
+
+Console.WriteLine(result);
+
